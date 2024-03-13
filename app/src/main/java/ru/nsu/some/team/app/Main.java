89484f91d2b6bf6b60430c6f5e749c81a731d0a3
Мельников Андрей <a.melnikov4@g.nsu.ru>
2024-03-13 10:26:31 +0700

    Make bMethod work

--- app/src/main/java/ru/nsu/some/team/app/Main.java
+++ app/src/main/java/ru/nsu/some/team/app/Main.java
@@ -1,9 +1,13 @@
 package ru.nsu.some.team.app;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException,
+            IllegalAccessException {
         A a = new A();
         a.aMethod();
+        a.getClass().getMethod("bMethod").invoke(a);
         // a.bMethod();
     }
 }
--- transformer/src/main/java/ru/nsu/some/team/transformer/MultipleInheritanceTransformer.java
+++ transformer/src/main/java/ru/nsu/some/team/transformer/MultipleInheritanceTransformer.java
@@ -1,101 +1,106 @@
 package ru.nsu.some.team.transformer;
 
-import javassist.CannotCompileException;
+import java.lang.instrument.ClassFileTransformer;
+import java.security.ProtectionDomain;
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtConstructor;
 import javassist.CtField;
 import javassist.CtMethod;
-import javassist.CtNewMethod;
-import javassist.Modifier;
 import javassist.NotFoundException;
-
-import java.io.IOException;
-import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
-import java.security.ProtectionDomain;
+import javassist.bytecode.Descriptor;
 
 public class MultipleInheritanceTransformer implements ClassFileTransformer {
-    @Override
-    public byte[] transform(
-            ClassLoader loader,
-            String className,
-            Class<?> classBeingRedefined,
-            ProtectionDomain protectionDomain,
-            byte[] classFileBuffer)
-            throws IllegalClassFormatException {
+  @Override
+  public byte[] transform(
+      ClassLoader loader,
+      String className,
+      Class<?> classBeingRedefined,
+      ProtectionDomain protectionDomain,
+      byte[] classFileBuffer) {
 
-        byte[] byteCode = classFileBuffer;
+//    byte[] byteCode = classFileBuffer;
 
-        if ("ru.nsu.some.team.app.Main".equals(className.replaceAll("/", "."))) {
+    //        if ("ru.nsu.some.team.app.Main".equals(className.replaceAll("/", "."))) {
+    //
+    //            try {
+    //                ClassPool pool = ClassPool.getDefault();
+    //                CtClass ctClass = pool.get("ru.nsu.some.team.app.Main");
+    //                CtMethod myMain = ctClass.getDeclaredMethod("main");
+    //                ctClass.removeMethod(myMain);
+    //
+    //                ctClass.addMethod(CtNewMethod.make("public static void main(String[] args) {
+    // System.out.println(\"Hello from replaced bytecode\");}", ctClass));
+    //
+    //                CtMethod[] methods = ctClass.getDeclaredMethods();
+    //
+    //                for (CtMethod method : methods) {
+    //                    System.out.println("!!!!!!! + " + method.getName());
+    //                    if (method.getName().equals("main")) {
+    //                        try {
+    //                            method.insertAfter("System.out.println(\"Logging using
+    // Agent\");");
+    //                        } catch (CannotCompileException e) {
+    //                            e.printStackTrace();
+    //                        }
+    //                    }
+    //                }
+    //                try {
+    //                    byteCode = ctClass.toBytecode();
+    //                    ctClass.detach();
+    //                    return byteCode;
+    //                } catch (IOException e) {
+    //                    e.printStackTrace();
+    //                }
+    //                ctClass.detach();
+    //                return byteCode;
+    //            } catch (NotFoundException e) {
+    //                System.out.println(e.getMessage());
+    //            } catch (CannotCompileException e) {
+    //                e.printStackTrace();
+    //            }
+    //
+    //        }
+    //        return byteCode;
 
-            try {
-                ClassPool pool = ClassPool.getDefault();
-                CtClass ctClass = pool.get("ru.nsu.some.team.app.Main");
-                CtMethod myMain = ctClass.getDeclaredMethod("main");
-                ctClass.removeMethod(myMain);
+    ClassPool cp = ClassPool.getDefault();
+    String effectiveClassName = className.replaceAll("/", ".");
+    try {
+      CtClass cClass = cp.get(effectiveClassName);
+      if (cClass.hasAnnotation(Extends.class)) {
+        Extends annotation = (Extends) cClass.getAnnotation(Extends.class);
+        for (Class<?> parent : annotation.value()) {
+          loader.loadClass(parent.getName());
+          CtClass parentCClass = cp.getCtClass(parent.getName());
 
-                ctClass.addMethod(CtNewMethod.make("public static void main(String[] args) { System.out.println(\"Hello from replaced bytecode\");}", ctClass));
+          String fieldName = "parent" + parent.getSimpleName();
+          CtField parentField =
+              CtField.make(String.format("protected %s %s;", parent.getName(), fieldName), cClass);
+          cClass.addField(parentField);
 
-                CtMethod[] methods = ctClass.getDeclaredMethods();
+          CtConstructor ctConstructor =
+              cClass.getConstructor(Descriptor.ofConstructor(new CtClass[] {}));
+          ctConstructor.insertBeforeBody(
+              String.format("this.%s = new %s();", fieldName, parent.getName()));
 
-                for (CtMethod method : methods) {
-                    System.out.println("!!!!!!! + " + method.getName());
-                    if (method.getName().equals("main")) {
-                        try {
-                            method.insertAfter("System.out.println(\"Logging using Agent\");");
-                        } catch (CannotCompileException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-                try {
-                    byteCode = ctClass.toBytecode();
-                    ctClass.detach();
-                    return byteCode;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                ctClass.detach();
-                return byteCode;
-            } catch (NotFoundException e) {
-                System.out.println(e.getMessage());
-            } catch (CannotCompileException e) {
-                e.printStackTrace();
-            }
-
+          for (CtMethod m : parentCClass.getDeclaredMethods()) {
+            CtMethod newMethod =
+                CtMethod.make(
+                    String.format(
+                        "public void %s() { this.%s.%s(); }", m.getName(), fieldName, m.getName()),
+                    cClass);
+            cClass.addMethod(newMethod);
+          }
         }
-        return byteCode;
 
-//    ClassPool cp = ClassPool.getDefault();
-//    String effectiveClassName = className.replaceAll("/", ".");
-//    try {
-//      CtClass cClass = cp.get(effectiveClassName);
-//      if (cClass.hasAnnotation(Extends.class)) {
-//        Extends annotation = (Extends) cClass.getAnnotation(Extends.class);
-//        for (Class<?> parent : annotation.value()) {
-//          loader.loadClass(parent.getName());
-//          CtClass parentCClass = cp.getCtClass(parent.getName());
-//
-//          String fieldName = "parent" + parent.getSimpleName();
-//          CtField parentField = CtField.make(String.format("protected %s %s;", parent.getName(), fieldName), cClass);
-//
-//          CtConstructor ctConstructor = cClass.getConstructor(Descriptor.ofConstructor(new CtClass[] {}));
-//          ctConstructor.insertBeforeBody(String.format("this.%s = new %s();", fieldName, parent.getName()));
-//
-//          for (CtMethod m : parentCClass.getDeclaredMethods()) {
-//            CtMethod newMethod = CtMethod.make(String.format("this.%s.%s();", fieldName, m.getName()), cClass);
-//            cClass.addMethod(newMethod);
-//          }
-//        }
-//
-//        byte[] newByteCode = cClass.toBytecode();
-//        cClass.detach();
-//        return newByteCode;
-//      }
-//    } catch (NotFoundException ignore) {
-//    } catch (Exception e) {
-//      e.printStackTrace();
-//    }
-        // return classFileBuffer;
+        byte[] newByteCode = cClass.toBytecode();
+        cClass.detach();
+        return newByteCode;
+      }
+    } catch (NotFoundException ignore) {
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    return classFileBuffer;
+  }
 }
